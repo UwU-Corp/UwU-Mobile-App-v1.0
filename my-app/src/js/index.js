68e622b0b08b1968f117397e0f10a2573b0fec06
Shinjitsu <71762653+shinjitsue@users.noreply.github.com>
2024-04-30 09:15:16 +0800

    hotel_info update

--- my-app/src/js/index.js
+++ my-app/src/js/index.js
@@ -1,8 +1,16 @@
-import { supabase, searchModal } from "./main";
+import {
+  supabase,
+  hotelImageUrl,
+  searchModal,
+  generateStarRating,
+} from "./main";
 
 // Import Images
 import fq_black from "../assets/icon/faq-fill-black.svg";
 import fq_white from "../assets/icon/faq-fill-white.svg";
+
+// Call the function to load recommended hotels
+getRecommendedHotels();
 
 // Modal for Search
 const modalSearch = document.getElementById("searchModal");
@@ -29,3 +37,54 @@ window.addEventListener("scroll", function () {
     faqImage.src = fq_white;
   }
 });
+
+// Function to get Recommended hotels from the database and display them
+async function getRecommendedHotels() {
+  // Fetching all hotels from the database
+  let { data: hotel, error } = await supabase
+    .from("hotel")
+    .select("*")
+    .order("id", { ascending: true });
+
+  // Getting the recommended element and adding the class 'scrolling-wrapper'
+  let recommended = document.getElementById("recommended");
+  recommended.classList.add("scrolling-wrapper");
+
+  // Shuffle the array of hotels
+  hotel.sort(() => Math.random() - 0.5);
+
+  // Select the first 5 hotels
+  let selectedHotels = hotel.slice(0, 5);
+
+  // Looping through each selected hotel and creating a div element for it
+  selectedHotels.forEach((element) => {
+    let hotelDiv = document.createElement("div");
+    hotelDiv.classList.add("col", "col_hotel");
+    hotelDiv.dataset.id = element.id;
+
+    // Convert hotel_rate to float
+    let hotelRate = parseFloat(element.hotel_rate);
+
+    // Setting the innerHTML of the div to the hotel details
+    hotelDiv.innerHTML = `
+      <a href="/hotel_info.html?id=${element.id}" class="card-link">
+        <div class="card" style="width: 18rem">
+          <img src="${
+            hotelImageUrl + element.hotel_imagepath
+          }" class="card-img-top" alt="..." />
+          <div class="card-body">
+            <p class="card-text">
+              ${generateStarRating(hotelRate)} ${hotelRate}
+              <span>(${element.no_reviews})</span>
+            </p>
+            <h5>${element.hotel_name}</h5>
+            <small>${element.hotel_location}, ${element.hotel_city}</small>
+            <h4 class="pt-2"><b>₱${element.price_range}</b></h4>
+          </div>
+        </div>
+      </a>`;
+
+    // Appending the div to the recommended element
+    recommended.appendChild(hotelDiv);
+  });
+}
--- my-app/src/js/main.js
+++ my-app/src/js/main.js
@@ -20,6 +20,13 @@ const supabase = createClient(
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFsemtqampidHlhcml1YnZjd2NuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTM5MzY4MDcsImV4cCI6MjAyOTUxMjgwN30.b7nqneAN1DXhQjilH1Xs5IhAZeVN1CjtYwfRzxZ87h8"
 );
 
+// URL for images
+const hotelImageUrl =
+  "https://alzkjjjbtyariubvcwcn.supabase.co/storage/v1/object/public/hotels/public/";
+
+const roomImageUrl =
+  "https://alzkjjjbtyariubvcwcn.supabase.co/storage/v1/object/public/rooms/public/";
+
 // Search functionality
 const searchModal = document.getElementById("searchModal");
 
@@ -33,4 +40,38 @@ if (searchModal) {
   });
 }
 
-export { supabase, searchModal };
+// Function to generate star rating
+function generateStarRating(rating) {
+  let stars = ""; // Initialize the stars string
+  // Calculate the number of full stars
+  let fullStars = Math.floor(rating);
+  // Calculate the decimal part of the rating
+  let decimalPart = rating - fullStars;
+
+  // Loop through 5 times for each star
+  for (let i = 1; i <= 5; i++) {
+    // If the current index is less than or equal to the number of full stars, add a full star
+    if (i <= fullStars) {
+      stars += '<i class="bi bi-star-fill"></i>';
+    }
+    // If the current index is equal to the number of full stars plus one and the decimal part is 0.5 or more, add a half star
+    else if (i === fullStars + 1 && decimalPart >= 0.5) {
+      stars += '<i class="bi bi-star-half"></i>';
+    }
+    // Otherwise, add an empty star
+    else {
+      stars += '<i class="bi bi-star"></i>';
+    }
+  }
+
+  // Return the stars string
+  return stars;
+}
+
+export {
+  supabase,
+  hotelImageUrl,
+  roomImageUrl,
+  searchModal,
+  generateStarRating,
+};
